40a281eedb3e0ad414a3a6d7e4b3e156733c16b5
Yue Zheng Ting <yuezhengting@gmail.com>
2022-12-18 18:59:24 +0800

    docstring for minimum spanning tree algorithms

---
+++ python3/tree/kruskal.py
@@ -0,0 +1,18 @@
+'''
+I actually learnt how it works but I have no idea how to even get started with the inputs... 
+Just gonna leave this docstring here for future me to update it when I am better.
+
+Kruskal's Algorithm is a greedy algorithm that continually finds uses the minimum cost edges
+to determine the minimum cost spanning tree. If the minimum cost edge selected will cause a
+cycle in the spanning tree, it will be discarded, and the next minimum cost edge will be used
+instead.
+
+While the typical implementation of Kruskal's Algorithm has a time complexity of O(n^2), it can
+be improved to O(n Log n) using min heaps.
+'''
+
+def minimum_cost_spanning_tree_kruskal():
+    # TODO: Implement the algorithm.
+    pass
+
+# TODO: Implement the input.
---
+++ python3/tree/prim.py
@@ -0,0 +1,16 @@
+'''
+I actually learnt how it works but I have no idea how to even get started with the inputs... 
+Just gonna leave this docstring here for future me to update it when I am better.
+
+Prim's Algorithm is a greedy algorithm that uses the following steps to determine the minimum spanning tree.
+1. Find the lowest cost edge first. This edge will determine the starting vertices.
+2. Find the lowest cost edge that are connected to the created tree.
+    (ie if edge (1,2) is selected in the first step, then the next edge selected has to be the lowest of those connected to vertice 1 and vertice 2.)
+3. Repeat step 2 until you achieve the required number of edges, which is |V| - 1, or number of vertices - 1.
+'''
+
+def minimum_cost_spanning_tree_prim():
+    # TODO: Implement the algorithm.
+    pass
+
+# TODO: Implement the input.
